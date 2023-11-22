b69cafe632063301769896902099cb8130fc95d0
arshad-hash <142026560+arshad-hash@users.noreply.github.com>
2023-11-22 13:45:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+let Inbutton= document.querySelector("input");
 function insert_Row() {
     //Write your code here
+	let table = document.getElementById("sampleTable");
+	let tr= document.createElement("tr");
+	let tdc1= document.createElement("td");
+	let tdc2= document.createElement("td");
+	tdc1.innerText="New Cell1";
+	tdc2.innerText="New Cell2"
+	tr.appendChild(tdc1);
+	tr.appendChild(tdc2);
+	
+	table.insertBefore(tr , table.firstChild);
+	
   
   
 }
